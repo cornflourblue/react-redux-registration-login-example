d07d691450c94492c48c7b7f1d06dc17017e5a2f
Jason Watmore <jason.watmore@hotmail.com>
2018-06-08 17:37:51 +1000

    added comment to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
         historyApiFallback: true
     },
     externals: {
+        // global app config object
         config: JSON.stringify({
             apiUrl: 'http://localhost:4000'
         })
